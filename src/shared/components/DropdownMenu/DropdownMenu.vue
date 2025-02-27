97a305febdd8d62424da6a37c57797f5887d7880
Ana Verena <agomesbc@gmail.com>
2025-02-26 22:26:15 -0300

    feature: add responsive layout to header component

--- src/shared/components/DropdownMenu/DropdownMenu.vue
+++ src/shared/components/DropdownMenu/DropdownMenu.vue
@@ -3,40 +3,45 @@
     <DropdownMenuTrigger as-child>
       <slot />
     </DropdownMenuTrigger>
-    <DropdownMenuContent class="p-0">
-      <DropdownMenuItem
-        @click="emit('addToList')"
-        v-for="item in menuList"
-        :key="item.id"
-        class="hover:bg-[#5e5974] px-6 py-3 hover:cursor-pointer flex items-center"
-      >
-        <i :class="item.icon" class="text-[#514889]" />
-        {{ item.title }}</DropdownMenuItem
-      >
+    <DropdownMenuContent class="p-0 bg-primary-100 border-none">
+      <template v-for="item in list" :key="item.value">
+        <Dialog v-if="item.dialog">
+          <DialogTrigger as-child>
+            <DropdownMenuItem
+              class="text-white hover:bg-[#5e5974] px-6 py-3 hover:cursor-pointer flex items-center"
+            >
+              <i :class="item.icon" class="text-[#514889]" v-if="item.icon" />
+              {{ item.label }}
+            </DropdownMenuItem>
+          </DialogTrigger>
+        </Dialog>
+        <DropdownMenuItem
+          v-else
+          @click="emit('onClick', item.value)"
+          class="text-white hover:bg-[#5e5974] px-6 py-3 hover:cursor-pointer flex items-center"
+        >
+          <i :class="item.icon" class="text-[#514889]" v-if="item.icon" />
+          {{ item.label }}
+        </DropdownMenuItem>
+      </template>
     </DropdownMenuContent>
   </DropdownMenu>
 </template>
 
+
 <script setup lang="ts">
+
+defineProps<{
+  list: any[];
+}>();
+
 import {
   DropdownMenu,
   DropdownMenuContent,
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from '@/shared/components/ui/dropdown-menu';
+import { Dialog, DialogTrigger } from '../ui/dialog';
 
-const emit = defineEmits(['addToList']);
-
-const menuList = [
-  {
-    id: 1,
-    title: 'Adicionar a lista',
-    icon: 'fa-solid fa-list',
-  },
-  // {
-  //   id: 2,
-  //   title: 'Ver detalhes',
-  //   icon: 'fa-solid fa-film',
-  // },
-];
+const emit = defineEmits(['onClick']);
 </script>
