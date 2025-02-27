2d6d659e6f9e839d5c48c73b63d6d284585f9790
Ana Verena <agomesbc@gmail.com>
2025-02-26 22:28:45 -0300

    feature: create list button in createDialog component

---
+++ src/shared/components/Button/CreateListButton/CreateListButton.vue
@@ -0,0 +1,20 @@
+<template>
+  <Button 
+    class="flex bg-primary-100 font-display mt-5 shadow-sm hover:cursor-pointer py-6 rounded-md justify-start"
+    @click="emit('onClick')"
+  >
+    <span
+      class="bg-secondary-100 rounded-md size-6 px-3 py-2 flex items-center justify-center"
+    >
+      <i class="fa-solid fa-plus text-white"></i>
+    </span>
+    <p class="text-sm font-display text-gray-400">Criar nova lista</p>
+  </Button>
+</template>
+
+<script setup lang="ts">
+import { defineEmits } from 'vue';
+import Button from '../../ui/button/Button.vue';
+
+const emit = defineEmits(['onClick']);
+</script>
