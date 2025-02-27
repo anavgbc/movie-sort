257d6074e3d6d3767e0c1757d06313537e48b326
Ana Verena <agomesbc@gmail.com>
2025-02-26 22:18:45 -0300

    feature: shared constants

---
+++ src/modules/lists/presenter/utils/feedbacks.ts
@@ -0,0 +1,8 @@
+export const listFeedbacks = Object.freeze({
+  SUCCESS_LIST_CREATED: 'Lista criada com sucesso.',
+  SUCCESS_LIST_DELETED: 'Lista deletada com sucesso.',
+  SUCCESS_LIST_MOVIE_UPDATED: 'Lista atualizada com sucesso.',
+  ERROR_LIST_CREATED: 'Erro ao criar lista.',
+  ERROR_FETCH_LISTS: 'Erro ao buscar listas.',
+
+});
